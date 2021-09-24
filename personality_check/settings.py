6ede26439818878365a13a080f001a62b81931d5
geraldbryan <geraldbryan9914@gmail.com>
2021-09-24 11:05:47 +0700

    fix deployment

--- personality_check/settings.py
+++ personality_check/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&$chp--8$(s6u$#=&l$uo8(#(v%c28re$w29n!oa-^^^jx9=6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','personalitytestapp.herokuapp.com']
 
