cb3e70cf52b70df860f3082087efc363bfedcb6b
geraldbryan <geraldbryan9914@gmail.com>
2021-09-24 15:01:02 +0700

    set debug false

--- personality_check/settings.py
+++ personality_check/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-&$chp--8$(s6u$#=&l$uo8(#(v%c28re$w29n!oa-^^^jx9=6t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','personalitytestapp.herokuapp.com']
 
